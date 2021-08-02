a30515f907d6af7b232d1f1292ad34ef7d03dbcb
David Ikin <shadow2272@aol.com>
2021-08-02 02:49:34 +0100

    drawBall() method firing & callback success

--- app.ts
+++ app.ts
@@ -1,53 +1,105 @@
-class Ball {
-    private size : number;
-    private xStartPos : number;
-    private yStartPos : number;
-    private xSpeed : number;
-    private ySpeed : number;
+import * as PIXI from 'pixi.js'
 
-    constructor(size: number, xStartPos: number, yStartPos: number,
-         xSpeed: number =6, ySpeed: number=6) { 
-      this.size = size;
-      this.xStartPos = xStartPos;
-      this.yStartPos = yStartPos;
-      this.xSpeed = xSpeed;
-      this.ySpeed = ySpeed;
+
+let ballSize = 32
+let ballPos = 0
+let speed = 6
+let appWidth = 500;
+let appHeight = 500;
+let ballColour = 0x0000FF;
+let app = new PIXI.Application({
+  backgroundColor: 0xffff00, // app works if yellow
+  height: appHeight,
+  width: appWidth
+});
+
+document.body.appendChild(app.view);
+
+class Ball {
+  private size : number;
+  private xStartPos : number;
+  private yStartPos : number;
+  private xSpeed : number;
+  private ySpeed: number;
+  private colour: number;
   
-    }
-  
-    getBallInfo() {
-      console.log(`ball size is ${this.size}`);
-      console.log(` X axis start position is ${this.xStartPos}`);
-      console.log(` Y axis start position is ${this.yStartPos}`);
-      console.log(`Speed is x${this.xSpeed} & y${this.ySpeed} `);
-    }
-    
-    placeBall() {
-      let randomPlace = Math.floor(Math.random() * (500 - 1 + 1)) + 1;
-      console.log(`xAxis = ${randomPlace} & yAxis = ${randomPlace}`);
-  
-    }
+  constructor(size, xStartPos, yStartPos, xSpeed, ySpeed, colour) { 
+    this.size = size;
+    this.xStartPos = xStartPos;
+    this.yStartPos = yStartPos;
+    this.xSpeed = xSpeed;
+    this.ySpeed = ySpeed;
+    this.colour = colour;
+    this.drawBall();
+
+  }
+
+  getBallInfo() {
+    console.log(`ball size is ${this.size}`);
+    let infoPosX = this.placeBall();
+    console.log(`Speed is x${this.xSpeed} & y${this.ySpeed}`);
+    console.log(`data for colour: ${this.colour}`);
   }
   
-  // let ballPos = Math.floor(Math.random() * (500 - 1 + 1)) + 1;
-  let ballPos : any
-  
-  let ballOne = new Ball(10, ballPos, ballPos);
-  let ballTwo = new Ball(20, ballPos, ballPos);
-  let ballThree = new Ball(30, ballPos, ballPos);
-  let ballFour = new Ball(40, ballPos, ballPos);
-  let ballFive = new Ball(50, ballPos, ballPos);
-  
-  
-  let ballContainer = [ballOne, ballTwo, ballThree, ballFour, ballFive];
-  
-  for(let i : number = 0; i < ballContainer.length; i++ ){
-    console.log(ballContainer[i].getBallInfo());
-    ballContainer[i].placeBall();
-  
+  placeBall() { 
+    let randomPlace = Math.floor(Math.random() * (499 - 1 + 1)) + 1;
+    console.log(`xAxis position = ${randomPlace} & yAxis = ${randomPlace}`) 
+    return randomPlace
+  }
+
+  drawBall() {
+    let testBall = new PIXI.Graphics();
+      testBall.beginFill(ballColour);
+      // it works the issue was above Graphics = graphics, thats why it did not work
+      testBall.drawCircle(this.placeBall(), this.placeBall(), ballSize);
+      testBall.endFill();
+      app.stage.addChild(testBall);
+      console.log(testBall);
+      return testBall
   }
   
-  //for(let ballInstance of ballContainer){
-  //  console.log(ballInstance.getBallInfo());
-  //  console.log(ballInstance.placeBall());
-  //}
+  testDisplay() {
+    let circle = new PIXI.Graphics();
+      circle.beginFill(0x9966FF);
+      circle.drawCircle(0, 0, 32);
+      circle.endFill();
+      circle.x = 64;
+      circle.y = 130;
+      app.stage.addChild(circle);
+  }
+}
+
+
+
+let ballOne = new Ball(ballSize, ballPos, ballPos, speed, speed, ballColour);
+let ballTwo = new Ball(ballSize, ballPos, ballPos, speed, speed, ballColour);
+let ballThree = new Ball(ballSize, ballPos, ballPos, speed, speed, ballColour);
+let ballFour = new Ball(ballSize, ballPos, ballPos, speed, speed, ballColour);
+let ballFive = new Ball(ballSize, ballPos, ballPos, speed, speed, ballColour);
+let ballSix = new Ball(ballSize, ballPos, ballPos, speed, speed, 0xFF0000)
+
+let ballContainer = [ballOne, ballTwo, ballThree, ballFour, ballFive, ballSix];
+
+for(let i = 0; i < ballContainer.length; i++ ){
+  console.log(ballContainer[i].getBallInfo());
+  ballContainer[i].placeBall();
+}
+
+// ballOne.testDisplay(); // a tester
+
+// function gameLoop(delta){
+//   if(ballOne.x >= appWidth - size || ballOne.x < size)
+//     ballOne.dx *= -1;
+//   if(ballOne.y >= appHeight - size)
+//     ballOne.dy = -18;
+  
+//   ballOne.x += ballOne.dx;
+//   ballOne.dy += 0.4;
+//   ballOne.y += ballOne.dy
+  
+// };
+
+// ballOne.dx = 4;
+// ballOne.dy = -0.8;
+
+// app.ticker.add(delta => gameLoop(delta));
